1e63eee7906e45b3f341483b2a1fbc59e2231317
99003779 <78867425+99003779@users.noreply.github.com>
2021-04-10 13:47:29 +0530

    Add files via upload

---
+++ Hands_on/param/param.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+int ndevices=1;
+module_param(ndevices,int,S_IRUGO);
+static int __init pdemo_init(void) 
+{
+	printk("Hello World..welcome\n");
+	return 0;
+}
+static void __exit pdemo_exit(void) 
+{
+	printk("Bye,Leaving the world %d\n",ndevices);
+}
+module_init(pdemo_init);
+module_exit(pdemo_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your name");
+MODULE_DESCRIPTION("Parameter demo Module");
+
